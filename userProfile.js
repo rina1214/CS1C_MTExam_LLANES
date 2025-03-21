eec7afce80f7199ce391c2b587bad6770d3aa765
rina1214 <rinagasconllanes@gmail.com>
2025-03-21 22:50:10 +0800

    Update userProfile.js

--- userProfile.js
+++ userProfile.js
@@ -2,3 +2,11 @@ let full_name = prompt("Your full Name:");
 let Age = prompt("Your Age:");
 let fav_number = prompt("Your Favorite Number:");
 let fav_color = prompt("Your Favorite Color:" );
+
+console.log(full_name);
+console.log(Age);
+console.log(fav_number);
+console.log(fav_color);
+
+
+
